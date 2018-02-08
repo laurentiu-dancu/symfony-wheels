d11ff689800155ce255259a7a97ee704e4689a20
Laurentiu Dancu <laurentiu.dancu@pitechnologies.ro>
2018-02-08 11:55:01 +0200

    Use encore for handling js and css.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
-/** If jQuery was cool, node would use it, amirite? **/
+import 'jquery';
+import 'bootstrap';
 
-// create the BLOG global variable if not existing already
-var BLOG = BLOG || {};
+// create the BLOG global letiable if not existing already
+let BLOG = BLOG || {};
 
 // add a function to the BLOG global
 BLOG.registerDeleteConfirmation = function() {
-    var deleteBtn = $('.delete-btn');
+    let deleteBtn = $('.delete-btn');
     // make sure we have delete buttons on the page
     if (deleteBtn.length == 0) {
         return;
@@ -14,7 +15,7 @@ BLOG.registerDeleteConfirmation = function() {
     // register an event listener on the delete button click
     deleteBtn.on('click', function(event) {
         // generate a confirm javascript alert
-        var isConfirmed = confirm('Are you sure you want to exist?');
+        let isConfirmed = confirm('Are you sure you want to exist?');
         /* of the response is false, prevent the clicking flow to continue,
          in this case the form submitting */
         if (!isConfirmed) {
@@ -24,7 +25,7 @@ BLOG.registerDeleteConfirmation = function() {
 };
 
 BLOG.handleCommentSubmit = function() {
-    var $replyButtons = $('.reply-btn');
+    let $replyButtons = $('.reply-btn');
     if ($replyButtons.length == 0) {
         return;
     }
@@ -33,11 +34,11 @@ BLOG.handleCommentSubmit = function() {
         // trigger modal
         $('#myModal').modal({});
         // get the pressed button element from the event
-        var $btn = $(event.currentTarget);
+        let $btn = $(event.currentTarget);
         // find comment main block, which hold's the id of the comment the user wants to reply
-        var $commentRoot = $btn.parents('.comment-details');
+        let $commentRoot = $btn.parents('.comment-details');
         // if there is no such a parent, we're trying to reply directly to the article
-        var commentIdToReply = $commentRoot.length > 0 ? $commentRoot.data('id') : null;
+        let commentIdToReply = $commentRoot.length > 0 ? $commentRoot.data('id') : null;
 
         // update the parent Id hidden input's value
         $('#comment_parentId').val(commentIdToReply);
@@ -45,16 +46,16 @@ BLOG.handleCommentSubmit = function() {
 };
 
 
-var interval = 120;
-var variation = 1;
+let interval = 120;
+let letiation = 1;
 
 BLOG.colorShift = function() {
-    var background = document.getElementById("background-image");
-    var logo = document.getElementById('logo-image');
-    var date = new Date();
-    var time = date.getTime();
+    let background = document.getElementById("background-image");
+    let logo = document.getElementById('logo-image');
+    let date = new Date();
+    let time = date.getTime();
 
-    var seed = time / interval % 360;
+    let seed = time / interval % 360;
     doShiftColor(background, seed);
     doShiftColor(logo, seed);
 };
@@ -63,25 +64,25 @@ function doShiftColor(element, degree) {
     if (degree > 360) {
         degree -= 360;
     }
-     element.style.filter = 'hue-rotate(' + degree + 'deg)';
+    element.style.filter = 'hue-rotate(' + degree + 'deg)';
     setTimeout(function () {
-        doShiftColor(element, degree + variation)
+        doShiftColor(element, degree + letiation)
     }, interval);
 }
 
 function floatAround() {
-    var background = document.getElementById("background-image");
-    var main = document.getElementById('main-container');
-    var date = new Date();
-    var time = date.getTime();
-    var seed = time / interval % 360;
+    let background = document.getElementById("background-image");
+    let main = document.getElementById('main-container');
+    let date = new Date();
+    let time = date.getTime();
+    let seed = time / interval % 360;
 
     doFloatAround(background, seed);
 }
 
 function doFloatAround(element, seed) {
 
-    var x = Math.sin(seed) * 1.6;
+    let x = Math.sin(seed) * 1.6;
     element.style.transform = 'skewY('+ x +'deg) translateY(-100px)';
     console.log(element);
 
