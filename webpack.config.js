2c13475153ab420824e7f7962a9d15738e0339d2
Laurentiu Dancu <laurentiu.dancu@pitechnologies.ro>
2018-02-08 14:26:04 +0200

    Preparations for react integration.

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,8 @@ Encore
     // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
 
+    .enableReactPreset()
+
     // will create web/build/app.js and web/build/app.css
     .addEntry('app', './assets/js/app.js')
     .addEntry('styles', './assets/scss/styles.scss')
