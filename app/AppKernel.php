09b5cd21d762f655140ac8f7a26c508f6fcc0d15
Laurentiu Dancu <laurentiu.dancu@pitechnologies.ro>
2018-02-26 17:33:01 +0200

    Update composer, install VichUploaderBundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new Vich\UploaderBundle\VichUploaderBundle(),
             new BlogBundle\BlogBundle(),
         ];
 
